bd182d3618ef37991af1cd0a1e380f85e4649525
Hammad Ahmed <Hammadahmedawan95@gmail.com>
2023-12-17 20:20:34 +0500

    Initial Commit

---
+++ app/src/main/java/com/alwaysondisplay/analogclock/digital/nightclock/App.kt
@@ -0,0 +1,9 @@
+package com.alwaysondisplay.analogclock.digital.nightclock
+
+import android.app.Application
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
